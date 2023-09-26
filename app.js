cdc2a15a6232e89467a07c3182a41948af490f17
Tasnim Jakir <tasnim748@gmail.com>
2023-09-26 13:50:40 +0600

    cors correction

--- app.js
+++ app.js
@@ -21,8 +21,22 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
+
+const allowedOrigins = ['http://localhost:4200'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  // You can configure other CORS headers here
+};
+app.use(cors(corsOptions));
+
 
 // routings
 app.use('/', indexRouter);
