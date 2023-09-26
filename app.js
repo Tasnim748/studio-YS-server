430d2ffe5203ed0a602fb75f1792e8903d86a9ae
Tasnim Jakir <tasnim748@gmail.com>
2023-09-26 20:54:52 +0600

    my commit

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+// var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 
@@ -18,9 +19,8 @@ app.set('view engine', 'jade');
 
 // app configurations
 app.use(logger('dev'));
-app.use(express.json({limit: '100mb'}));
-app.use(express.urlencoded({limit: '100mb', extended: true }));
-app.use(cookieParser());
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
