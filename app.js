bbb5ab9a30b0176f1ad06577f104fa1902350d5f
Tasnim Jakir <tasnim748@gmail.com>
2023-09-26 20:50:14 +0600

    my commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'jade');
 // app configurations
 app.use(logger('dev'));
 app.use(express.json({limit: '100mb'}));
-app.use(express.urlencoded({limit: '100mb', extended: false }));
+app.use(express.urlencoded({limit: '100mb', extended: true }));
 app.use(cookieParser());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
